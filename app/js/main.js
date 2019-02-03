029db5d73370a86d145498421630239641ce5942
Aliaksey Sachavichyk <roim@ya.ru>
2019-02-03 16:09:51 +0300

    fix reload index

--- app/js/main.js
+++ app/js/main.js
@@ -3,7 +3,4 @@
 var myObject = {
     a: 0,
     b: 1,
-    c: 2,
-    d: 3,
-    f: 4,
 };
--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,12 @@
 "use string";
-
-var browserSync  = require('browser-sync');         // Подключаем Browser Sync
-var del          = require('del');                  // Удаление файлов и папок
-var gulp         = require('gulp');                 // Gulp пакет
-var gulpAutopref = require('gulp-autoprefixer');    // Автопрефексер для CSS
-var gulpCleanCSS = require('gulp-clean-css');       // Минификация CSS файлов
-var rename       = require('gulp-rename');          // Переименования файлов
-var gulpScss     = require('gulp-sass');            // Подключаем Sass пакет
-var gulpUglify   = require('gulp-uglify');          // Минификация JS файлов
+var browserSync    = require('browser-sync');         // Подключаем Browser Sy
+var gulp           = require('gulp');                 // Gulp пакет
+var gulpAutopref   = require('gulp-autoprefixer');    // Автопрефексер для CSS
+var gulpCache      = require('gulp-cache');           // Работа с кэшем
+var gulpCleanCSS   = require('gulp-clean-css');       // Минификация CSS файлов
+var rename         = require('gulp-rename');          // Переименования файлов
+var gulpScss       = require('gulp-sass');            // Подключаем Sass пакет
+var gulpUglify     = require('gulp-uglify');          // Минификация JS файлов
 
 
 
@@ -20,15 +19,14 @@ const paths = {
 };
 
 var clean = function(){
-    return del([ paths.css + '*.css' ]);    // Удаляем все указанные файлы CSS
+    return gulpCache.clearAll();    // Очищаем кэш
 };
 
 var scss = function(){
-    return gulp.src( paths.scss + 'main.scss' )          // Берем источник
+    return gulp.src( paths.scss + '**/*.scss' )          // Берем источник
            .pipe( gulpScss() )                           // Преобразуем Scss в CSS
            .pipe( gulpAutopref(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: false }) ) // Добавляем префексы
            .pipe( gulp.dest(paths.css) )                 // Выгружаем результат
-           .pipe( browserSync.reload({stream: true}) )   // Обновляем CSS на странице при изменении
 };
 
 var minCSS = function(){
@@ -40,27 +38,29 @@ var minCSS = function(){
 };
 
 var minJS = function(){
-    return gulp.src( paths.js + 'main.js' )        // Все JS файлы
+    return gulp.src( paths.js + 'main.js' )     // Все JS файлы
            .pipe( gulpUglify() )                // Минифицируем файл
            .pipe(rename({suffix: '.min'}))      // Добавляем суффикс .min
            .pipe( gulp.dest( paths.js ) )       // Выгружаем все в текущий каталог
-           .pipe( browserSync.reload({stream: false}) )   // Обновляем CSS на странице при изменении
 };
 
 var browser = function(){
-    browserSync({                   // Выполняем browser Sync
-        server: {                   // Определяем параметры сервера
-            baseDir: paths.app },   // Директория для сервера - app
-        notify: false               // Отключаем уведомления
+    browserSync({ // Выполняем browserSync
+        server: { // Определяем параметры сервера
+            baseDir: paths.app // Директория для сервера - app
+        },
+        notify: false // Отключаем уведомления
     });
 };
 
+
 var watch = function(){ // Следим за изменениями файлов
+    clean();
     browser();
-    gulp.watch( paths.scss + 'main.scss', scss );
+    gulp.watch( paths.scss + '**/*.scss', scss );
     gulp.watch( paths.css + 'main.css', minCSS );
-    gulp.watch( paths.js + 'main.js', minJS );
-    gulp.watch( paths.app + '*.html', browserSync.reload );
+    gulp.watch( paths.js + 'main.js', minJS ).on('change', browserSync.reload);
+    gulp.watch( paths.app + '*.html').on('change', browserSync.reload);
 };
 
 gulp.task('clean', clean);
